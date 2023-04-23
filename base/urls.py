5d480db2b6722d9c57db59f25e075b80eaf48146
AteyaShuborna <ateya.shuborna98@gmail.com>
2023-04-23 22:48:57 +0600

    minox fix and more test run

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.WelcomePage.as_view(), name='index'),
-    path('home/',views.Homepage.as_view(), name = 'home'),
+    path('',views.welcome, name='index'),
+   
 ]
--- base/views.py
+++ base/views.py
@@ -4,14 +4,10 @@ from django.views import View
 from post.models import AdoptionPost, MissingPost
 
 # Create your views here.
-class WelcomePage(View):
-    def get(self,request):
-        a_pets = AdoptionPost.objects.filter(pet_availability=True)
-        m_pets =  MissingPost.objects.filter(pet_still_missing= True)
-        context={'missing_pets':m_pets, 'adoption_pets': a_pets}
-        return render(request,'index.html', context)
-    
-class Homepage(View):
-    def get(self,request):
-        return render (request,'homepage.html')
+def welcome(request):
+    a_pets = AdoptionPost.objects.filter(pet_availability=True)
+    m_pets =  MissingPost.objects.filter(pet_still_missing= True)
+    context={'missing_pets':m_pets, 'adoption_pets': a_pets}
+    return render(request,'index.html', context)
+
 
